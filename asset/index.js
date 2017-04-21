8c58b66ea9f2dcb9657d70366e41de6545738d81
wurui <wurui.wr@gmail.com>
2017-04-21 17:39:32 +0800

    first

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,164 @@
-define([],function(){
-  return {
-    init:function($mod){
+define(['zepto', './carjson.js'], function (undef, CarList) {
+    var settings = {
+        tpl: 1,
+        bgcolor: 1,
+        // color1:1
+        text1: $('#text1input').val(),
+        text2: $('#text2input').val()
+    };
+    var $carlist;
+    var unshownList = [];
+    var renderNextScroll = function () {//10 by 10
+        var el = $carlist[0],
+            sl = el.scrollLeft,
+            w = el.clientWidth,
+            sw = el.scrollWidth;
+        if(sw-2*w-sl<0){
+
+            var l = Math.min(10,unshownList.length);
+            var html='';
+            while (l--) {
+                var n = unshownList.shift();
+
+                html += '<label class="carlogo"><img title="' + n + '" src="' + carlogoPath(n) + '" width="50"/><br/>' +
+                    '<input type="radio" name="carlogo" ' + (settings.carlogo == n ? 'checked="checked"' : '') + ' value="' + n + '"/></label>'
+
+            }
+            $carlist.append(html);
+        }else{
+            //console.log('1111')
+        }
+        //console.log($carlist[0].clientWidth, $carlist[0].scrollWidth, sl)
+    };
+    var carlogoPath = function (name) {
+        return 'http://www.shaomachetie.com/static/smct/img/carlogo/' + name + '.jpg';
+    };
+    var renderCarList = function (searchKey) {
+        var searchKey = searchKey || '';
+        var html = '';
+
+        var list = CarList;
+        var i = 0;
+        var firstScreenCount=20;
+        unshownList=[];
+        while (i < list.length) {
+            var n = list[i++];
+            if (searchKey && n.indexOf(searchKey) == -1) {
+                continue
+            }
+            if(firstScreenCount){
+                firstScreenCount--;
+                html += '<label class="carlogo"><img title="' + n + '" src="' + carlogoPath(n) + '" width="50"/><br/>' +
+                    '<input type="radio" name="carlogo" ' + (settings.carlogo == n ? 'checked="checked"' : '') + ' value="' + n + '"/></label>'
+            }else{
+                unshownList.push(n);
+            }
+
+        }
+        if (html && !searchKey) {
+            html = '<label class="carlogo">不使用<br/>车标<br/><input type="radio" ' + (!settings.carlogo ? 'checked="checked"' : '') + ' name="carlogo" value=""/></label>' + html
+        }
+        $carlist.html(html || '无结果')[0].scrollLeft=0;
+    };
+    var initColor = function ($node) {
+
+        var s = '', i = 0;
+        while (i < 12) {
+            i++;
+            s += '<label class="bgcolor bgcolor-' + i + '"><input type="radio"' + (i == 1 ? 'checked="checked"' : '') + ' name="bgcolor" value="' + i + '"/></label>'
+        }
+        $node.html(s)
 
     }
-  }
+    return {
+        init: function ($mod) {
+
+
+            var $toolbar = $('.J_toolbar', $mod).on('change', function (e) {
+                var tar = e.target;
+                var name = tar.name,
+                    val = tar.value;
+
+                //console.log(name,val);
+                switch (name) {
+                    case 'tpl':
+                        $('#TPL').removeClass('tpl-' + settings.tpl).addClass('tpl-' + val)
+                        settings.tpl = val;
+                        break
+                    case 'bgcolor':
+                        $('#preview').removeClass('bgcolor-' + settings.bgcolor).addClass('bgcolor-' + val)
+                        settings.bgcolor = val;
+                        break
+                    case 'color1':
+                        $('#mainText').css('color', $(tar).closest('label').css('backgroundColor'))
+                        settings.color1 = val;
+                        break
+                    case 'text1':
+
+                        $('#text1').html(tar.value)
+                        settings.text1 = val;
+                        break
+                    case 'text2':
+                        $('#text2').html(tar.value)
+                        settings.text2 = val;
+                        break
+                    case 'carlogo':
+                        $('#central').html(val ? '<img src="' + carlogoPath(val) + '"/>' : '');
+                        settings.carlogo = val;
+                        break
+                    case 'carkey':
+                        renderCarList(val)
+                        break
+                }
+
+            }).on('tap', '[data-switch]', function (e) {
+
+                //var toolbar=document.getElementById('toolbar');
+                var type = e.target.getAttribute('data-switch');
+                $toolbar.attr('data-on', type);
+                location.href = '#toolbar'
+
+            });
+
+            var TO;
+            $carlist = $('.J_carlist', $toolbar).on('scroll', function () {
+                if (TO)clearTimeout(TO);
+                TO = setTimeout(renderNextScroll, 100)
+            });
+
+            $($mod).on('submit',function(e){
+                var f= e.target;
+
+
+                for(var k in settings){
+                    var hid=document.createElement('input');
+                    hid.type='hidden';
+                    hid.name=k;
+                    hid.value=settings[k];
+                    f.appendChild(hid)
+                }
+                return false;
+
+
+
+            });
+
+
+
+
+
+            initColor($('.J_Colors', $mod))
+            renderCarList();
+
+
+        }
+    }
 })
+
+
+/**
+ * Created by wurui on 01/04/2017.
+ */
+
+
+
--- mod.xsl
+++ mod.xsl
@@ -1,10 +1,89 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oxm="https://www.openxsl.com">
     <xsl:template match="/root" name="wurui.smct-build">
+        <xsl:param name="actionShopcart">http://www./smct/submitbuild</xsl:param>
+        <xsl:param name="actionWorks">/smct/submitbuild</xsl:param>
         <!-- className 'J_OXMod' required  -->
         <div class="J_OXMod oxmod-smct-build" ox-mod="smct-build">
-            <h1>
-                This is mod smct-build;
-            </h1>
+
+            <a name="toolbar"></a>
+            <section class="toolbar J_toolbar" data-on="1">
+                <dl>
+                    <dt>选择背景颜色</dt>
+                    <dd class="tpl-list J_Colors">
+                        颜色加载中...
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>选择车标&#160;&#160;&#160;&#160;
+                        <input type="search" name="carkey" placeholder="筛选条件,如:宝马"/>
+                    </dt>
+                    <dd class="tpl-list">
+
+                        <div class="carlist J_carlist"></div>
+
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>文案编辑</dt>
+                    <dd class="tpl-list">
+                        <datalist id="text1option">
+                            <option>扫码移车</option>
+                            <option>扫码挪车</option>
+                            <option>扫码通知车主</option>
+                            <option>有事请扫码</option>
+                        </datalist>
+                        <datalist id="text2option">
+                            <option>www.shaomachetie.com</option>
+                            <option>杭州声罄科技有限公司出品</option>
+                        </datalist>
+
+                        头部文案:
+                        <input type="text" id="text1input" name="text1" size="30" autocomplete="off" value="扫码通知车主"
+                               list="text1option"
+                               maxlength="6" placeholder="扫码通知车主"/>
+                        <br/>
+                        <br/>
+                        底部文案:
+                        <input type="text" id="text2input" name="text2" size="30" list="text2option"
+                               value="www.shaomachetie.com"
+                               maxlength="30" placeholder="www.shaomachetie.com"/>
+                    </dd>
+                </dl>
+                <div class="switches">
+                    <!-- <button type="button" onclick="showToolbar(1)">选择模版</button> -->
+                    <button type="button" data-switch="1">背景颜色</button>
+                    <button type="button" data-switch="2">车标</button>
+                    <button type="button" data-switch="3">文案</button>
+                </div>
+            </section>
+
+            <section>
+
+                <h3 class="title">效果预览</h3>
+
+                <div class="preview bgcolor-1" id="preview">
+                    <div id="text1" class="card-header">扫码通知车主</div>
+                    <div id="TPL" class="card-body tpl tpl-1">
+                        <div class="central" id="central"></div>
+                        <img src="http://i.oxm1.cc/uploads/git/wurui/img/2ahkwkkveTj1rgh0ueRlcquA5vz-1000.png"
+                             class="qrcode" width="120"/>
+                    </div>
+                    <div class="card-footer">
+                        <span id="text2">www.shaomachetie.com</span>
+                    </div>
+
+                </div>
+
+                <div class="buttons">
+                    <form method="post" action="{$actionShopcart}">
+                        <button type="submit" class="bt-main">我很满意,提交!</button>
+
+                        <button type="submit" onclick="this.form.action='{$actionWorks}'" class="bt-second">保存至我的车贴</button>
+                    </form>
+                </div>
+
+
+            </section>
         </div>
     </xsl:template>
 </xsl:stylesheet>
